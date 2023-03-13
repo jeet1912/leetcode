dc0e76e47a42d00cb15e57a72e9fbb1ca5d0e86e
jeet1912 <sahdevjeet@gmail.com>
2023-03-13 19:47:49 +0530

    prefix_sum

--- arrays_and_strings/prefix_sum.py
+++ arrays_and_strings/prefix_sum.py
@@ -5,17 +5,43 @@ class PrefixSum:
     # A query is true if the sum of the subarray from x to y is less than limit, or
     #  false otherwise.
     # Time complexity - O(n+m) where n is length of nums and m is length of queries
-    def problem1(nums,queries,limit):
-        prefix_sum = nums[0]
+    def problem1(self,nums,queries,limit):
+        prefix_sum = [nums[0]]
         for i in range(1,len(nums)):
             prefix_sum.append(nums[i] + prefix_sum[-1])
         ans = []
         for x,y in queries: 
             sum = prefix_sum[y] - prefix_sum[x] + nums[x]
             ans.append(sum<limit)
+        return ans
 
-    def numWaysToSplitArray():
-        pass
+    # Given an integer array nums, find the number of ways to split the array into two parts 
+    # so that the first section has a sum greater than or equal to the sum of
+    #  the second section. The second section should have at least one number.
+    def numWaysToSplitArray(self,nums):
+        prefix_sum = [nums[0]]
+        for i in range(len(nums)):
+            prefix_sum.append(nums[i] + prefix_sum[-1])
+        ans = 0
+        for r in range(len(nums)-1):
+            left_sum = prefix_sum[i]
+            right_sum = prefix_sum[-1] - prefix_sum[i]
+            if left_sum >= right_sum:
+                ans+=1
+        return ans
+    #Time complexity - O(n) - space complexity
+    # in O(1) - space complexity
+    def numWaysToSplitArray2(self,nums):
+        ans = total = 0
+        total = sum(nums)
+        for r in range(len(nums)-1):
+            left_sum += nums[r]
+            right_sum = total - left_sum
+            if left_sum >= right_sum:
+                ans +=1
+        
 
 ps = PrefixSum()
-t = ps.problem1()
+n = [10,4,-8,7]
+t = ps.problem1([1, 6, 3, 2, 7, 2],[[0, 3], [2, 5], [2, 4]],13)
+print(t)
