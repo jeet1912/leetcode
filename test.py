1d977f934951e5291db78ee569731419312223ff
Sahdev Abhijeet <sv.xxt@Sahdevs-MacBook-Pro.local>
2025-04-16 16:37:58 +0530

    recursion 2

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
-def iterative(n):
-    for i in range(n):
-        print(i+1)
 
-def recursive(n):
-    if n > 10:
-        print("done")
-        return
-    print(n)
-    recursive(n+1)
-    print("is it here")
-    
+if __name__ == "__main__":
+    print("Hello World")
+    def iterative(n):
+        for i in range(n):
+            print(i+1)
 
-#iterative(10)
-recursive(1)
+    def recursive(n):
+        if n > 10:
+            print(f"done, n is {n}")
+            return
+        print(n)
+        recursive(n+1)
+        print("is it here")
+
+    #iterative(10)
+    recursive(1)
