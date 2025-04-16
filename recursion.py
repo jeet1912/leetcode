0331f0f7badec4d22e044003c2dbff53130e4252
Sahdev Abhijeet <sv.xxt@Sahdevs-MacBook-Pro.local>
2025-04-16 17:03:12 +0530

    fibonacci

--- recursion.py
+++ recursion.py
@@ -1,15 +1,31 @@
-def iterative(n):
-    for i in range(n):
-        print(i+1)
 
-def recursive(n):
-    if n > 10:
-        print(f"done, n is {n}")
+class Recursion: 
+
+    def iterative(n):
+        for i in range(n):
+            print(i+1)
+
+    def recursive(self, n):
+        if n > 10:
+            print(f"done, n is {n}")
+            return
+        print(n)
+        self.recursive(n+1)
+        print("what is the value of n? ", n)
         return
-    print(n)
-    recursive(n+1)
-    print("what is the value of n? ", n)
-    return
 
-#iterative(10)
-recursive(0)
+    # Where recursion shines is when you use it to break down a problem into "subproblems", 
+    # whose solutions can then be combined to solve the original problem.
+
+    def fibonacci(self,n):
+        if n <= 1:
+            return n
+        else:
+            return self.fibonacci(n-1) + self.fibonacci(n-2)
+
+r = Recursion()
+print(r.fibonacci(4))
+        
+
+
+
