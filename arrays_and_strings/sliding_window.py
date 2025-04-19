a1d754cb750abffedb60e677e9da0fd2b3a493b3
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-19 16:11:46 +0530

    question 2

--- arrays_and_strings/sliding_window.py
+++ arrays_and_strings/sliding_window.py
@@ -14,5 +14,20 @@ class SlidingWindow:
     # Time complexity is O(n)
     # Space complexity is O(1)
     
+    def question2(self, nums: list[int]) -> tuple[int, list[int]]:
+        l = numZ = ans = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                numZ += 1
+            while numZ > 1:
+                if nums[l] == 0:
+                    numZ -= 1
+                l += 1
+            ans = max(ans, i - l + 1)
+            arr = nums[l:i+1]
+        return ans, arr
+    # Time complexity is O(n)
+    # Space complexity is O(1)
+
 s = SlidingWindow()
-print(s.question1([3,2,1,3,1,1],k=5))
+print(s.question2([1,1,0,0,1,0,1,1]))
