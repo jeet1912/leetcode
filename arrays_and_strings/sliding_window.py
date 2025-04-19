c776fc2301ecd4384acc864bf96be8469a8dda68
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-19 17:26:52 +0530

    question 4 fixed window

--- arrays_and_strings/sliding_window.py
+++ arrays_and_strings/sliding_window.py
@@ -42,7 +42,19 @@ class SlidingWindow:
                 l += 1
             ans += r-l+1
         return ans 
+    # Time complexity is O(n)
+    # Space complexity is O(1)
+
+    def largestSumSubArray(self, nums: list[int], k: int) -> int:
+        curr = 0
+        for i in range(k):
+            curr += nums[i]
+        max_sum = curr
+        for i in range(k, len(nums)):
+            curr += nums[i] - nums[i-k]
+            max_sum = max(max_sum, curr)
+        return max_sum
 
 s = SlidingWindow()
-print(s.numSubArrayProductLessThanK([10,5,2,6],k=100))
+print(s.largestSumSubArray([10,5,2,6],k=3))
 
