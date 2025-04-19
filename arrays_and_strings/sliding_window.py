b73e037af0cd237eb6ac6f9c673d767160f59983
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-19 15:44:59 +0530

    q1

--- arrays_and_strings/sliding_window.py
+++ arrays_and_strings/sliding_window.py
@@ -1,4 +1,16 @@
 class SlidingWindow:
 
     def question1(self, nums : list[int], k: int) -> list[int]:
-        pass
+        l = r = ans = sum = 0
+        for i in range(nums):
+            sum += nums[i]
+            while sum > k:
+                sum -= nums[l]
+                l += 1
+            ans = max(ans, i - l + 1)
+        return ans
+    # Time complexity is O(n)
+    # Space complexity is O(1)
+    
+s = SlidingWindow()
+print(s.question1([3,2,1,3,1,1],k=5))
