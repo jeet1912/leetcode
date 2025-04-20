e5ccd9d173aa09c552460ee8c617b8398902b16e
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-20 16:57:05 +0530

    min start value - prefix sum

--- arrays_and_strings/prefix_sum.py
+++ arrays_and_strings/prefix_sum.py
@@ -2,7 +2,7 @@ class PrefixSum:
     
     def answerQueries(self, nums: list[int], queries: list[int], limit: int) -> list[bool]:
         prefix = [nums[0]]
-        for i in range(len(nums)):
+        for i in range(1,len(nums)):
             prefix.append(nums[i]+prefix[-1])
         # Time complexity is O(n)
         result = []
@@ -15,8 +15,45 @@ class PrefixSum:
                 result.append(True)
         return result
         # Time complexity is O(m)
-
         # Net time complexity is O(n+m)
+
+    def question2(self, nums: list[int]) -> int:
+        prefix = [nums[0]]
+        for i in range(1, len(nums)):
+            prefix.append(nums[i]+prefix[-1])
+        print(prefix)
+        ans = 0
+        for i in range(len(nums)-1):
+            left = prefix[i]
+            print("left ",left)
+            right = prefix[-1] - prefix[i+1] + nums[i+1]
+            print("right ",right)
+            if left >= right:
+                ans += 1
+        return ans
+        # Time complexity is O(n)
+        # Space complexity is O(n)
+
+    def question2b(self, nums: list[int]) -> int:
+        count = curr = 0
+        total = sum(nums)
+        for i in range(len(nums)-1):
+            curr += nums[i]
+            if curr >= total - curr:
+                count += 1
+        return count
+        # Time complexity is O(n)
+        # Space complexity is O(1)
+
+    def minStartValue(self, nums: list[int]) -> int:
+        prefix = [nums[0]]
+        for i in range(1,len(nums)):
+            prefix.append(nums[i]+prefix[-1])
+        minPre = min(prefix)
+        if minPre > 0:
+            return 1
+        else:
+            return -minPre + 1     
     
 ps = PrefixSum()
-print(ps.answerQueries([1,2,3,4], [[0,1],[1,2],[2,3]], 3))
+print(ps.question2b([10,4,-8,7]))
