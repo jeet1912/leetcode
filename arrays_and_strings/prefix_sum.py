34f053e627705e3959f15a7b1c80b12235ca789f
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-20 15:44:11 +0530

    answer queries - prefix sum

--- arrays_and_strings/prefix_sum.py
+++ arrays_and_strings/prefix_sum.py
@@ -1,6 +1,22 @@
 class PrefixSum:
-    pass
     
-    
+    def answerQueries(self, nums: list[int], queries: list[int], limit: int) -> list[bool]:
+        prefix = [nums[0]]
+        for i in range(len(nums)):
+            prefix.append(nums[i]+prefix[-1])
+        # Time complexity is O(n)
+        result = []
+        for query in queries:
+            i , j = query
+            sum = prefix[j] - prefix[i] + nums[i]
+            if sum > limit:
+                result.append(False)
+            else:
+                result.append(True)
+        return result
+        # Time complexity is O(m)
+
+        # Net time complexity is O(n+m)
     
 ps = PrefixSum()
+print(ps.answerQueries([1,2,3,4], [[0,1],[1,2],[2,3]], 3))
