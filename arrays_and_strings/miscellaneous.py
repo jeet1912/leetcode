ddc42cb30d82fbf813d5be532f1330d099d04852
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-21 15:26:29 +0530

    reverse only letters

--- arrays_and_strings/miscellaneous.py
+++ arrays_and_strings/miscellaneous.py
@@ -31,7 +31,23 @@ class Solution:
             words[i] = words[i][::-1]
         return " ".join(words)
         # Time complexity is O(n)
-        # Space complexity is O(n)
+        # Space complexity is O(n)]
+
+    def reverseOnlyLetters(self, s: str) -> str:
+        l = 0
+        r = len(s) - 1
+        s = list(s)
+        while l < r:
+            if not s[l].isalpha():
+                l+= 1
+            elif not s[r].isalpha():
+                r-=1
+            else:        
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+        return "".join(s)
+
     
 s = Solution()
 ans = s.reverseWords('leetcode rules')
