c5ec6a10f9dd63e3138f77a96d1ddcd426aca7fd
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-21 15:13:23 +0530

    reverse words

--- arrays_and_strings/miscellaneous.py
+++ arrays_and_strings/miscellaneous.py
@@ -7,9 +7,33 @@ class Solution:
     
     def findIndexOfFirstOccuranceInString(self, haystack, needle):
         return haystack.index(needle) if needle in haystack else -1
-            
+    
+    def reverseWords(self, s: str) -> str:
+        words = s.split(" ")
+        ans = ""
+        for i, word in enumerate(words):
+            word = list(word)
+            l = 0
+            r = len(word)-1
+            while l<r:
+                word[l], word[r] = word[r], word[l]
+                l += 1
+                r -= 1
+            if i == 0:
+                ans = "".join(word)
+            else:
+                ans += " " + "".join(word)
+        return ans
+    
+    def reverseWords2(self, s: str) -> str:
+        words = s.split(" ")
+        for i in range(len(words)):
+            words[i] = words[i][::-1]
+        return " ".join(words)
+        # Time complexity is O(n)
+        # Space complexity is O(n)
     
 s = Solution()
-ans = s.findIndexOfFirstOccuranceInString('leetcode','code')
+ans = s.reverseWords('leetcode rules')
 print(ans)
     
