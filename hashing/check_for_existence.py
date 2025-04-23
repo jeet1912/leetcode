15809c51484d71200bb12ab40bc3f273a8d3f028
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-23 17:44:43 +0530

    two sum using hash table

--- hashing/check_for_existence.py
+++ hashing/check_for_existence.py
@@ -1,7 +1,16 @@
 
 class Hashing:
-    pass
-
+    
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        t = {}
+        for i in range(len(nums)):
+            val = target - nums[i]
+            if val in t:
+                return t[val], i
+            t[nums[i]]=i
+    # Time complexity is O(n)
+    # Space complexity is O(n)
            
 h = Hashing()
+print(h.twoSum([2,7,11,15], 9)) # [0,1]
 
