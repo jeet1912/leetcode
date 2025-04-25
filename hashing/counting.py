20a1d71a9d95a6e1a697736ec60823fed53a012d
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-25 15:33:37 +0530

    intersection of arrays

--- hashing/counting.py
+++ hashing/counting.py
@@ -14,6 +14,18 @@ class Counting:
                 left+=1
             ans = max(0,right-left+1)
         return ans
-            
+
+    def intersectionOfArrays(self, nums: list[list[int]]) -> list[int]:
+        d = defaultdict(int)
+        intersection = []
+        for array in nums:
+            for x in array:
+                d[x] += 1
+                if d[x] == len(nums):
+                    intersection.append(x)
+        return sorted(intersection)
+
+
+
 c = Counting()
-print(c.lengthOfSubstringWithKdistinctChar("eceba",k=2))
+print(c.intersectionOfArrays([[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]))
