2bdd3db8de49598dd891e74d757bc76e720faf0e
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-25 14:55:29 +0530

    length of substring with k distinct characters

--- hashing/counting.py
+++ hashing/counting.py
@@ -1,8 +1,19 @@
 from collections import defaultdict, Counter
 
 class Counting:
-    pass
     
-
+    def lengthOfSubstringWithKdistinctChar(self, s:str, k: int) -> int:
+        d = defaultdict(int)
+        left = ans = 0
+        for right in range(len(s)):
+            d[s[right]] += 1
+            while len(d) > k:
+                d[s[left]] -= 1
+                if d[s[left]] == 0:
+                    del d[s[left]]
+                left+=1
+            ans = max(0,right-left+1)
+        return ans
+            
 c = Counting()
-
+print(c.lengthOfSubstringWithKdistinctChar("eceba",k=2))
