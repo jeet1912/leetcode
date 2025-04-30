170f1aeed38ba6f8543898ecb573680921bd7adf
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-30 17:54:03 +0530

    sliding window with a hash map to solve min consecutive cards to pick up

--- hashing/miscellaneous.py
+++ hashing/miscellaneous.py
@@ -8,7 +8,18 @@ class Miscellaneous:
             sortedSt = "".join(sorted(str))
             hashMap[sortedSt].append(str)
         return list(hashMap.values())
+    
+    def minConsecutiveCardToPick(self, cards: list[int]) -> int:
+        seen = {}
+        minLen = float('inf')
+        for r in range(len(cards)):
+            if cards[r] in seen:
+                minLen = min(minLen, r - seen[cards[r]] + 1)
+            seen[cards[r]] = r
+        
+        return minLen if minLen!= float('inf') else -1
             
+    #def minConsecutiveCardToPick2(self, cards:)
     
 m = Miscellaneous()
-print(m.returnAnagramsFromArray(strs = ["eat","tea","tan","ate","nat","bat"]))
+print(m.minConsecutiveCardToPick(cards=[1,2,6,2,1]))
