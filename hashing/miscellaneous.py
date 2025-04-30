e60c4eb76b0c514e19e5dc7f03cd9cb0f6f9ccb8
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-30 18:44:16 +0530

    max sum of pair with equal sum of digits

--- hashing/miscellaneous.py
+++ hashing/miscellaneous.py
@@ -2,12 +2,14 @@ from collections import defaultdict, Counter
 
 class Miscellaneous:
     
+
     def returnAnagramsFromArray(self, strs: list[str]) -> list[list[str]]:
         hashMap = defaultdict(list)
         for str in strs:
             sortedSt = "".join(sorted(str))
             hashMap[sortedSt].append(str)
         return list(hashMap.values())
+    
     
     def minConsecutiveCardToPick(self, cards: list[int]) -> int:
         seen = {}
@@ -18,7 +20,8 @@ class Miscellaneous:
             seen[cards[r]] = r
         
         return minLen if minLen!= float('inf') else -1
-            
+
+
     # or length of the shortest subarray        
     def shortestDistanceBetweenDuplicates(self, nums: list[int]) -> int:
         hashMap = defaultdict(list)
@@ -29,9 +32,32 @@ class Miscellaneous:
         for values in hashMap.values():
             for i in range(len(values)-1):
                 minValue = min(minValue, values[i+1] - values[i] + 1)
-        
+        #Still O(n) since it iterates over all the indices of the array of length n.
         return minValue if minValue!= float('inf') else -1
 
+
+
+    def sumOfDigits(self, num: int) -> int:
+        digitSum = 0
+        while num>0:
+            digitSum += num%10
+            num //= 10
+        return digitSum
     
+    
+    def maxSumOfPairWithEqualSumOfDigits(self, nums:list[int]) -> int:
+        d = defaultdict(list)
+        for num in nums:
+            d[self.sumOfDigits(num=num)].append(num)
+        maxValue = 0
+        for values in d.values():
+            if len(values)>1:
+                values = sorted(values)
+                maxValue = max(maxValue,values[-1] + values[-2])
+        return maxValue if maxValue!=0 else -1
+    
+
+
+
 m = Miscellaneous()
-print(m.shortestDistanceBetweenDuplicates(nums=[1,2,6,2,1]))
+print(m.maxSumOfPairWithEqualSumOfDigits(nums=[18,43,36,13,7]))
