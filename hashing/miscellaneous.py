2afb599d71e6c47e77d8de46e6ecc292e10109ec
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-04-30 16:37:22 +0530

    return anagrams from a list of strings

--- hashing/miscellaneous.py
+++ hashing/miscellaneous.py
@@ -1,7 +1,14 @@
 from collections import defaultdict, Counter
 
 class Miscellaneous:
-    pass
+    
+    def returnAnagramsFromArray(self, strs: list[str]) -> list[list[str]]:
+        hashMap = defaultdict(list)
+        for str in strs:
+            sortedSt = "".join(sorted(str))
+            hashMap[sortedSt].append(str)
+        return list(hashMap.values())
+            
     
 m = Miscellaneous()
-print(m)
+print(m.returnAnagramsFromArray(strs = ["eat","tea","tan","ate","nat","bat"]))
