ca7a3845c9e9791a1f43451ea19c5475b39860e1
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-05-06 18:57:49 +0530

    custom sort string

--- hashing/counting.py
+++ hashing/counting.py
@@ -318,6 +318,11 @@ class Counting:
                 wordToPattern[word] = char
         return True
             
+    def customSortString(self, order:str, s:str) -> str:
+        orderMap = {char: i for i, char in enumerate(order)}
+        sorted_S = sorted(s, key= lambda char: orderMap.get(char,len(order)))
+        return "".join(sorted_S)
+            
 
 c = Counting()
-print(c.wordPattern(pattern="abba",s="dog cat cat dog dog"))
+print(c.customSortString(order="cba",s="abcd"))
