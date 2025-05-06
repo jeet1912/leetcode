ba5f2b0e7d43138db5c784459f69eb909124ca12
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-05-06 19:13:08 +0530

    close strings

--- hashing/counting.py
+++ hashing/counting.py
@@ -322,7 +322,11 @@ class Counting:
         orderMap = {char: i for i, char in enumerate(order)}
         sorted_S = sorted(s, key= lambda char: orderMap.get(char,len(order)))
         return "".join(sorted_S)
-            
+    
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        c1 = Counter(word1)
+        c2 = Counter(word2)
+        return (c1.keys() == c2.keys()) and sorted(c1.values()) == sorted(c2.values())
 
 c = Counting()
 print(c.customSortString(order="cba",s="abcd"))
