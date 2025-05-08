41cc07ed2ad4167b1420f2dfa7d53abd6418891e
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-05-08 19:52:50 +0530

    adding to a singly linked list

--- linked_list/singly.py
+++ linked_list/singly.py
@@ -10,14 +10,36 @@ class ListNode:
             head = head.next
         return self.sum
 
+    def getSum(self, head):
+        # Using recursion
+        if not head:
+            return 0
+        return head.val + self.getSum(head.next)
+    
+    def addToEnd(self, node1, node):
+        if node1.next != None:
+            print("Sorry, try another approach.")
+        node1.next = node
+    
+    def addInBetween(self, node1, node):
+        node.next = node1.next
+        node1.next = node
+
 
 one = ListNode(1)
 two = ListNode(2)
 three = ListNode(3)
+four = ListNode(4)
+five = ListNode(5)
+head = one
 one.next = two
 two.next = three
 #print(one.next.val)
-print(two.traversal(head=one))
+three.addToEnd(three,four)
+head.addInBetween(three,five)
+print(three.next.val)
+print(five.next.val)
+print(two.getSum(head))
 
 
 
