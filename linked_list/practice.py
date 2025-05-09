6ff12309fa1a1955e88608df775dc3759fd869a7
Abhijeet Sahdev <sahdevjeet@gmail.com>
2025-05-09 17:08:49 +0530

    whether a list has a cycle or not

--- linked_list/practice.py
+++ linked_list/practice.py
@@ -9,6 +9,24 @@ class Practice:
             fast = fast.next.next
         return slow.val
     
+    def hasCycle(self, head) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+    
+        '''
+        seen = set()
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
+        '''
 
 l = SinglyLinkedList()
 l.append(1)
@@ -16,4 +34,4 @@ l.append(2)
 l.append(3)
 
 p = Practice()
-print(p.getMiddle(l.head))
+print(p.hasCycle(l.head))
